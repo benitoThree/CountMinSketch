7528f2bfa9bc108538e294ffaf2ec19668c0bf19
BenGeordie <school@bengeordie@gmail.com>
2022-02-16 21:54:55 -0600

    cleaner : )

--- src/time_series.rs
+++ src/time_series.rs
@@ -1,10 +1,7 @@
-use crossbeam::epoch::Atomic;
 use rand;
-use std::{thread};
-use std::sync::{Arc};
 use std::sync::atomic::{AtomicU32, Ordering};
-use crossbeam::thread::scope;
 use array_init::array_init;
+use rayon::prelude::*;
 
 pub struct CountMinSketch<const A: usize, const B: usize> {
     arr: [[AtomicU32; B]; A],
@@ -14,49 +11,33 @@ pub struct CountMinSketch<const A: usize, const B: usize> {
 impl <const A: usize, const B: usize> CountMinSketch<A, B> {
     pub fn new() -> Self {
         Self { 
-            arr: array_init(|_| array_init(|_| AtomicU32::new(0))), 
-            hash_constants: [(rand::random::<usize>(), rand::random::<usize>()); A], 
+            arr: array_init(|_| {
+                array_init(|_| AtomicU32::new(0))
+            }), 
+            hash_constants: [(rand::random(), rand::random()); A], 
         }
     }
 
-    pub fn batch_index<const X: usize>(self: &mut Self, x_arr: &[u32; X]) {
-        let self_arc = Arc::new(self);
-        scope(|scope| {
-            // let self_arr = Arc::new(self.arr);
-            x_arr.map(|x| {
-                let self_arc = Arc::clone(&self_arc);
-                scope.spawn(move |_| {
-                    let indices = self_arc.getIndices(x);
-                    for (i, &idx) in indices.iter().enumerate() {
-                        self_arc.arr[i][idx].fetch_add(1, Ordering::Relaxed);
-                    }
-                })
-            }).map(|handle| {
-                handle.join().unwrap()
-            })
-        }).expect("Threads failed.");
+    pub fn batch_index(self: &mut Self, x_arr: &Vec<u32>) {
+        x_arr.par_iter().for_each(|&x| {
+            self.get_indices(x).iter().enumerate().for_each(|(i, &idx)| {
+                self.arr[i][idx].fetch_add(1, Ordering::Relaxed);
+            });
+        });
     }
 
-    pub fn batch_query<const X: usize>(self: &Self, x_arr: &[u32;X]) -> [u32;X] {
-        let self_arc = Arc::new(self);
-        scope(|scope| {
-            // let self_arr = Arc::new(self.arr);
-            x_arr.map(|x| {
-                let self_arc = Arc::clone(&self_arc);
-                scope.spawn(move |_| {
-                    let indices = self_arc.getIndices(x);
-                    indices.iter().enumerate().map(|(i, &idx)| {
-                        self_arc.arr[i][idx].load(Ordering::Relaxed)
-                    }).reduce(u32::min).unwrap()
-                })
-            }).map(|handle| {
-                handle.join().unwrap()
-            })
-        }).unwrap()
+    pub fn batch_query(self: &Self, x_arr: &Vec<u32>) -> Vec<u32> {
+        x_arr.par_iter().map(|&x| {
+            self.get_indices(x).iter().enumerate().map(|(i, &idx)| {
+                self.arr[i][idx].load(Ordering::Relaxed)
+            }).reduce(u32::min).unwrap()
+        }).collect()
     }
 
-    fn getIndices(self: & Self, x: u32) -> [usize; A] {
-        self.hash_constants.map(|(a, b)| (a.wrapping_mul(x as usize).wrapping_add(b)) % B)
+    fn get_indices(self: & Self, x: u32) -> [usize; A] {
+        self.hash_constants.map(|(a, b)| {
+            (a.wrapping_mul(x as usize).wrapping_add(b)) % B
+        })
     }
 }
 
